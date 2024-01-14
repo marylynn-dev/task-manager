bb908420ce376529bbd7eeefeaca758d2dd81c1c
Marylynn <marlymaingi28@gmail.com>
2024-01-14 19:52:38 +0300

    added netlify functions

--- src/store/task-store.js
+++ src/store/task-store.js
@@ -21,16 +21,22 @@ export const useTaskStore = defineStore('taskStore', {
     // Action to asynchronously fetch tasks from the server
     async getTasks() {
       this.isLoading = true;
-      const res = await fetch('/.netlify/functions/mockApi');
-      const data = await res.json();
-      this.tasks = data;
-      this.isLoading = false;
+      try {
+        const res = await fetch('/.netlify/functions/mock-api');
+        const data = await res.json();
+        this.tasks = data.tasks;
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.isLoading = false;
+
+      }
     },
 
     // Action to asynchronously add a new task
     async addTask(task) {
       this.tasks.push(task);
-      const res = await fetch('/.netlify/functions/mockApi', {
+      const res = await fetch('/.netlify/functions/mock-api', {
         method: 'POST',
         body: JSON.stringify(task),
         headers: { 'Content-Type': 'application/json' }
@@ -41,7 +47,7 @@ export const useTaskStore = defineStore('taskStore', {
     // Action to asynchronously delete a task
     async deleteTask(id) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
-      const res = await fetch(`/.netlify/functions/deleteTask/${id}`, {
+      const res = await fetch(`/.netlify/functions/mock-api/deleteTask/${id}`, {
         method: 'DELETE'
       });
       if (res.error) { console.log(res.err); }
@@ -52,7 +58,7 @@ export const useTaskStore = defineStore('taskStore', {
       const task = this.tasks.find((task) => task.id === id);
       task.isFav = !task.isFav;
 
-      const res = await fetch(`/.netlify/functions/toggleFav/${id}`, {
+      const res = await fetch(`/.netlify/functions/mock-api/toggleFav/${id}`, {
         method: 'PATCH',
         body: JSON.stringify({ isFav: task.isFav }),
         headers: { 'Content-Type': 'application/json' }
@@ -63,10 +69,18 @@ export const useTaskStore = defineStore('taskStore', {
     // Action to asynchronously reset tasks
     async reset() {
       this.tasks = [];
-      const res = await fetch('/.netlify/functions/resetTasks', {
+      const res = await fetch('/.netlify/functions/mock-api/resetTasks', {
         method: 'DELETE'
       });
       if (res.error) { console.log(res.err); }
     }
   }
 });
+
+//reason why the db data doesn't persists when hosted
+
+// Netlify functions cannot take any kind of POST, PUT, PATCH, UPDATE, or DELETE call… Because it’s only a FUNCTION :rofl:
+
+// It took a couple of days to get this through my thick skull. It can respond to GET… but only with hard-coded data. Once it is built/deployed, it’s just a functional old funky function.
+
+// If a function could take in new data (mutate state), it wouldn’t be a function! Because then f(x) wouldn’t always give the same result.
